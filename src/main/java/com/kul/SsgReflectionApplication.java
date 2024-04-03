cd8a1c18b895b25fdc3d48c32ac51448a1f46c23
wushuai <659-4gpe145ln@dingtalk.com>
2024-04-03 17:41:25 +0800

    【项目初始化】

---
+++ src/main/java/com/kul/SsgReflectionApplication.java
@@ -0,0 +1,13 @@
+package com.kul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgReflectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgReflectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kul/SsgReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgReflectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
